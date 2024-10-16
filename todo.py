36fcaa2666dc30ee888b00bdfd35178ee6d25424
Thiago M Souza <46378621+mtsz-thiago@users.noreply.github.com>
2024-10-16 11:22:22 -0300

    tinha deletado as funções erradas

--- todo.py
+++ todo.py
@@ -4,6 +4,8 @@ import os
 
 tasks_list = []
 
+tasks_list = []
+
 def add_task(task):
     tasks_list.append(task)
 
@@ -11,13 +13,13 @@ def list_tasks():
     pass
 
 def load_tasks():
-    pass
+    global tasks_list
+    if os.path.exists('./data/tasks.pkl'):
+        with open('./data/tasks.pkl', 'rb') as f:
+            tasks_list = pickle.load(f)            
 
 def save_tasks():
-    if not os.path.exists('./data'):
-        os.makedirs('./data')
-    with open('./data/tasks.pkl', 'wb') as f:
-        pickle.dump(tasks_list, f)
+    pass
 
 def main():
     parser = argparse.ArgumentParser(description="Simple TODO CLI")
